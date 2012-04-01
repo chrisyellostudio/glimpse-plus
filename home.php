328d4d560c0cf72d4dd152edfbf536d63d8aa054
ChristopherOwenRees@gmail.com <ChristopherOwenRees@gmail.com@4eb8b699-a952-e42b-50ec-16c201ea6325>
2012-04-01 16:23:47 +0000

    Added some contents

--- home.php
+++ home.php
@@ -1,33 +1,38 @@
 <?php
 
-
 /**
  * Description of Introduction
  *
  * @author cir8
  */
-
 include 'ApplicationWebBody.php';
 include 'ApplicationWebPage.php';
 
 class home {
 
-    public static function createPage(){        
-        $links = array('intro.php'=>'Introduction',
-            'tour.php'=> 'Guided Tour');
-        $currentLocation = array('home.php'=>'Home');
-        $bodyContent = '';
-        
-        $body = new ApplicationWebBody('Home',$bodyContent);
+    public static function createPage() {
+        $links = array('intro.php' => 'Introduction',
+            'tour.php' => 'Guided Tour');
+        $currentLocation = array('home.php' => 'Home');
+        $bodyContent = '        
+        <p>Welcome to GlimPSE! Your resource for added functionality and features!</p>
+        <h2>What is this about</h2>
+        <p>This site does</p>
+        <h2>Hello</h2>
+        <p>Something else goes down here...</p>';
+
+        $body = new ApplicationWebBody('Home', $bodyContent);
         $body->setCurrentBranch('home');
         $body->setbreadArray($currentLocation);
         $body->setRightContentLinks($links);
-        
+
         $page = new ApplicationWebPage();
-        echo $page->head('Home');
+        echo $page->head('Home', '', $scripts);
         echo $page->body($body);
         echo $page->footer();
     }
+
 }
+
 session_start();
 home::createPage();
