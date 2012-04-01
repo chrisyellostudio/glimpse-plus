6f067a40a54aaffdd743d69f7baccb09d365c260
ChristopherOwenRees@gmail.com <ChristopherOwenRees@gmail.com@4eb8b699-a952-e42b-50ec-16c201ea6325>
2012-04-01 16:22:00 +0000

    Changes to include location on registration

--- Application.php
+++ Application.php
@@ -6,27 +6,36 @@
  * @author cir8
  */
 class Application {
-
     /**
      * Constructor for the Application configuration.
      * 
      * @param string $base_dir 
      */
-    
     const BASE_DIR = '/';
+
+    public function __construct() {
+        $this->getUserInfo();
+    }
     
     public function constructConfig() {
         $this->base_dir = BASE_DIR;
-
         $this->conf['db'] = array();
         $this->conf['user'] = array();
     }
-    
+
     /*
      * 
      */
-    public function getUserInfo(){
+
+    public function getUserInfo() {
+        $gi = geoip_open('GeoIP.dat', GEOIP_MEMORY_CACHE);
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $country = geoip_country_code_by_addr($gi, $ip);
+        geoip_close($gi);
         
+        session_start();
+        $_SESSION['country'] = $country;
+        $_SESSION['ip'] = $ip;       
     }
 }
 
